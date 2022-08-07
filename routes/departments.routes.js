d6f56f21f3a0448a2799d1db62db4fcfa70761de
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-07 17:12:44 +0300

    change message OK to object to update and delete

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -51,7 +51,7 @@ router.put('/departments/:id', async (req, res) => {
         { _id: req.params.id },
         { $set: { name: name } }
       );
-      res.json({ message: 'OK' });
+      res.json(dep);
     } else {
       res.status(404).json({ message: 'Not found...' });
     }
@@ -65,7 +65,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json(dep);
     } else {
       res.status(404).json({ message: 'Not found...' });
     }
